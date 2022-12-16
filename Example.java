e5dcf3df96145d88053b43d1fe1f4f8e01fa9a8e
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2022-12-16 11:39:54 +0700

    16/12/65 11.38 - first commit

--- Example.java
+++ Example.java
@@ -1,3 +1,8 @@
 publc class Example {
-	public static void main (String [] args)
-		System.out.println ("Hello World")
+	public static void main (String [] args) {
+		System.out.println ("Hello World") ;
+	}
+	public void myMethod (){
+		System.out printLn ("Nanthawat Phoosaengsan")
+	}
+}
